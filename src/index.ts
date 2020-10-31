47cc448094d01949d2d501e40bfd8db7356b73c8
Orie Steele <orie@transmute.industries>
2020-10-31 15:30:05 -0500

    chore: updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export const sum = (a: number, b: number) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('boop');
-  }
-
-  return a + b;
-};
+export * from './makeAuthorizedRequest';
+export * from './verifyAuthorizedRequest';
